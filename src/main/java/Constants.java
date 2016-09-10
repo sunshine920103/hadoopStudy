29bbbb08a0fe0d5f0e9be39ac70ce3892ff6468c
cong.chen <cong.chen@wbkit.com>
2016-09-10 14:19:54 +0800

    读取hbase内容

---
+++ src/main/java/Constants.java
@@ -0,0 +1,16 @@
+/**
+ * 使用的一些常量
+ *
+ * Created by cc on 2016/9/7.
+ */
+public class Constants {
+    public static final   String COLUMN_FAMILY = "data";
+
+    private static final String NAMESPACE="cc:";
+
+    public static class TableName{
+        public static final  String TEST=NAMESPACE + "test";
+    }
+
+
+}
--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,35 +1,66 @@
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.NullOutputFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2016/7/21.
  */
 public class WordCount {
 
+    private static final Logger LOGGER= LoggerFactory.getLogger(WordCount.class);
+
+    static class HbaseMapper extends TableMapper<Text,Text>{
+
+        @Override
+        protected void map(ImmutableBytesWritable row, Result result, Context context) throws IOException, InterruptedException {
+
+            byte[] b = row.get();
+            String keys = Bytes.toString(b);
+            byte[] value=result.getValue(Bytes.toBytes(Constants.COLUMN_FAMILY),Bytes.toBytes("f1"));
+
+            LOGGER.info("------keys---::::"+keys);
+            LOGGER.info("------values---::::"+Bytes.toString(value));
+
+        }
+    }
+
+
+
     public static void main(String[] args) throws Exception
     {
+        Configuration config = HBaseConfiguration.create();
+        Job job = Job.getInstance(config,"mytest");
+        job.setJarByClass(WordCount.class);     // class that contains mapper
+
+        Scan scan = new Scan();
+
+        //每次rpc的恳求记录数
+        scan.setCaching(500);        // 1 is the default in Scan, which will be bad for MapReduce jobs
+        scan.setCacheBlocks(false);  // don't set to true for MR jobs
+
+        TableMapReduceUtil.initTableMapperJob(
+                Constants.TableName.TEST,        // input HBase table name
+                scan,             // Scan instance to control CF and attribute selection
+                HbaseMapper.class,   // mapper
+                null,             // mapper output key
+                null,             // mapper output value
+                job);
+        job.setOutputFormatClass(NullOutputFormat.class);   // because we aren't emitting anything from mapper
 
 
-        Job job=Job.getInstance();
-        job.setJarByClass(WordCount.class);
 
-        job.setMapperClass(WcMap.class);
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(LongWritable.class);
-
-
-        job.setReducerClass(WcReduce.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(LongWritable.class);
-
-
-        //设置输入和输出目录
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
